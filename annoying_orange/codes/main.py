7204817f3d3a87b01217cd2c0a08127f97bd7fd3
fidabspd <fidabspd@gmail.com>
2022-01-12 22:05:41 +0900

    Add comments

--- annoying_orange/codes/main.py
+++ annoying_orange/codes/main.py
@@ -4,7 +4,7 @@ from imutils import face_utils, resize
 import numpy as np
 from moviepy.editor import VideoFileClip
 
-SOURCES_DIR = '../resources/'
+REOURCES_DIR = '../resources/'
 RESULT_DIR = '../result/'
 SIZE = (1753, 1753)
 EYE_WIDTH = 342
@@ -15,13 +15,13 @@ MOUTH_POS = (616, 1096)
 FRAME = 29.96
 
 
-orange_img = cv2.imread(SOURCES_DIR+'orange.jpg')
+orange_img = cv2.imread(REOURCES_DIR+'orange.jpg')
 orange_img = cv2.resize(orange_img, dsize=SIZE)
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(SOURCES_DIR+'shape_predictor_68_face_landmarks.dat')
+predictor = dlib.shape_predictor(REOURCES_DIR+'shape_predictor_68_face_landmarks.dat')
 
-cap = cv2.VideoCapture(SOURCES_DIR+'why55_origin.mov')
+cap = cv2.VideoCapture(REOURCES_DIR+'why55_origin.mov')
 
 img_array = []
 
@@ -45,6 +45,7 @@ while cap.isOpened():
         shape = predictor(img, face)
         shape = face_utils.shape_to_np(shape)
 
+        # face_landmarks 68개 위치에 원을 그림. 반지름이 2이므로 사실상 점.
         # for p in shape:
         #     cv2.circle(face_img, center=(p[0] - x1, p[1] - y1), radius=2, color=255, thickness=-1)
 
@@ -122,7 +123,7 @@ for i in range(len(img_array)):
 out.release()
 
 
-videoclip = VideoFileClip(SOURCES_DIR+"why55_origin.mov")
+videoclip = VideoFileClip(REOURCES_DIR+"why55_origin.mov")
 audioclip = videoclip.audio
 
 new_videoclip = VideoFileClip(RESULT_DIR+"why55_chimrangeee_no_audio.avi")
